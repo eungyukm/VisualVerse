1951a94c06f50fdbcc3ab0b696eeef18146e1d19
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2024-11-26 21:04:31 +0900

    feat: Add authentication system with login, logout, and register funcionality
    - Implemented CustomUser model by exending AbstractUser
    - Configured AUTH_USER_MODEL to use 'loginmanager.CustomUser'
    - Defined URL patterns for authentication routes (login, logout, register)
    - Created templates for login, register, and home pages
    - Updated project URLs to include authentiaction app

--- vv_core/loginmanager/urls.py
+++ vv_core/loginmanager/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'loginmanager'
+
 urlpatterns = [
-    path('login/', views.login_view, name='login'),  # 로그인 페이지
-    path('logout/', views.logout_view, name='logout'),  # 로그아웃
-    path('register/', views.register_view, name='register'),  # 회원가입 페이지
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- vv_core/vv_core/urls.py
+++ vv_core/vv_core/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('auth/', include('loginmanager.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
