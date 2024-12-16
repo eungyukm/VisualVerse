a8c05af07c5b70eaacd59f9e396cf2b80799fea5
eungyukim <eungyukm@gmail.com>
2024-12-17 00:11:02 +0900

    upload Images files

--- vv_core/storymanager/views.py
+++ vv_core/storymanager/views.py
@@ -1,4 +1,21 @@
+from django.conf import settings
 from django.shortcuts import render
+from postmanager.models import Post
 
 def index(request):
-    return render(request, 'storymanager/index.html')
+    # Fetch all posts and their associated images
+    posts_with_images = Post.objects.prefetch_related('images').all()
+
+    # Prepare data for rendering
+    post_cards = []
+    for post in posts_with_images:
+        # Check if the post has at least one associated image
+        if post.images.exists():
+            post_image = f"{settings.MEDIA_URL}{post.images.first().image}"
+            post_cards.append({
+                "title": post.title,
+                "image_url": post_image,
+                "slug": post.slug,
+            })
+
+    return render(request, 'storymanager/index.html', {"post_cards": post_cards})
